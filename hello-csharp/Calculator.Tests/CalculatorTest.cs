706de8b67dae01bd01fbe039c3fbc5f40a773c4f
ThisBodenmann <89189853+ThisBodenmann@users.noreply.github.com>
2022-10-26 13:13:37 +0200

    Aufgabe 1-3

--- hello-csharp/Calculator.Tests/CalculatorTest.cs
+++ hello-csharp/Calculator.Tests/CalculatorTest.cs
@@ -1,13 +1,39 @@
+using System;
 using Xunit;
 
 namespace Calculator.Tests
 {
-    public class UnitTest1
+    public class CalculatorTest
     {
         [Fact]
-        public void FivePlusThreeIsEight()
+        public void TestAddThreeToTwoIsFive()
         {
-            Assert.Equal(8, Calculator.Add(3, 5));
+            Assert.Equal(5, Calculator.Add(3, 2));
+        }
+
+        [Fact]
+        public void TestSubtractFourFromSixIsTwo()
+        {
+            Assert.Equal(2, Calculator.Subtract(6, 4));
+        }
+
+        [Fact]
+        public void TestMultiplyTwoAndThreeIsSix()
+        {
+            Assert.Equal(6, Calculator.Multiply(3, 2));
+        }
+
+        [Fact]
+        public void TestDivideEightAndTwoIsFour()
+        {
+            Assert.Equal(4, Calculator.Divide(8, 2));
+        }
+
+        [Fact]
+        public void TestDivideWithZero()
+        {
+
+            Assert.Throws<ArgumentException>(() => Assert.Equal(0, Calculator.Divide(8, 0)));
         }
     }
 }
--- hello-csharp/Calculator/Calculator.cs
+++ hello-csharp/Calculator/Calculator.cs
@@ -1,10 +1,31 @@
-﻿namespace Calculator
+﻿using System;
+
+namespace Calculator
 {
     public class Calculator
     {
         public static int Add(int a, int b)
         {
             return a + b;
+        }
+
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static int Divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new ArgumentException();
+            }
+            return a / b;
         }
     }
 }
