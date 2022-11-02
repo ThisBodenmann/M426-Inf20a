5a5efc19c630da333e2393a59e36e7f073214a05
ThisBodenmann <89189853+ThisBodenmann@users.noreply.github.com>
2022-11-02 13:32:09 +0100

    02-tdd

--- Aufgaben/02-tdd/FooBarQix.Tests/FooBarQixDeterminerTest.cs
+++ Aufgaben/02-tdd/FooBarQix.Tests/FooBarQixDeterminerTest.cs
@@ -5,8 +5,58 @@ namespace FooBarQix.Tests
     public class FooBarQixDeterminerTest
     {
         [Fact]
-        public void Test1()
+        public void TestForOne()
         {
+            string expected = "1";
+            FooBarQixDeterminer determiner = new FooBarQixDeterminer();
+
+            string actual = determiner.Determine(1);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestForThree()
+        {
+            string expected = "FooFoo";
+            FooBarQixDeterminer determiner = new FooBarQixDeterminer();
+
+            string actual = determiner.Determine(3);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestForSix()
+        {
+            string expected = "Foo";
+            FooBarQixDeterminer determiner = new FooBarQixDeterminer();
+
+            string actual = determiner.Determine(6);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestForTwentyOne()
+        {
+            string expected = "FooQix";
+            FooBarQixDeterminer determiner = new FooBarQixDeterminer();
+
+            string actual = determiner.Determine(21);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestForOneHundredAndFive()
+        {
+            string expected = "FooBarBarQix";
+            FooBarQixDeterminer determiner = new FooBarQixDeterminer();
+
+            string actual = determiner.Determine(105);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
--- Aufgaben/02-tdd/FooBarQix/FooBarQixDeterminer.cs
+++ Aufgaben/02-tdd/FooBarQix/FooBarQixDeterminer.cs
@@ -1,7 +1,24 @@
-﻿namespace FooBarQix
+﻿using System;
+
+namespace FooBarQix
 {
     public class FooBarQixDeterminer
     {
+        public string Determine(int input)
+        {
+            string result = "";
 
+            if (input % 3 == 0) { result = "Foo"; }
+            if (Convert.ToString(input).Contains("3")) { result += "Foo"; }
+            if (input % 5 == 0) { result += "Bar"; }
+            if (Convert.ToString(input).Contains("5")) { result += "Bar"; }
+            if (input % 7 == 0) { result += "Qix"; }
+            if (Convert.ToString(input).Contains("7")) { result += "Qix"; }
+
+
+
+            if (result == "") { return Convert.ToString(input); }
+            return result;
+        }
     }
 }
